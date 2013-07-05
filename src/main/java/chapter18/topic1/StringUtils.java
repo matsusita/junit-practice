e63b643f9334d36cf12a7d83561ae7a3b1f196a3
matsusita <matsusita@e-mind.co.jp>
2013-07-05 17:32:48 +0900

    file add
    StringUtils追加。

---
+++ src/main/java/chapter18/topic1/StringUtils.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package chapter18.topic1;
+
+/**
+ * @author matsusita
+ *
+ */
+public class StringUtils {
+	
+	/**
+	 * @param string
+	 * @return
+	 */
+	public String toSnakeCase(String string) {
+		return "aaa";
+	}
+	
+}
--- src/test/java/chapter18/topic1/StringUtilsTest.java
+++ src/test/java/chapter18/topic1/StringUtilsTest.java
@@ -3,11 +3,21 @@
  */
 package chapter18.topic1;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 /**
  * @author matsusita
  *
  */
 public class StringUtilsTest {
+	
+	@Test
+	public void aaaを入力するとaaaが取得できる() throws Exception {
+		StringUtils sut = new StringUtils();
+		assertThat(sut.toSnakeCase("aaa"), is("aaa"));
+	}
 	
 }
