f85ff14c916ef19e1e2b295717d475dcee5a8dea
matsusita <matsusita@e-mind.co.jp>
2013-07-24 17:56:42 +0900

    modified topic6

--- src/main/java/chapter18/topic6/BackgroundTask.java
+++ src/main/java/chapter18/topic6/BackgroundTask.java
@@ -3,16 +3,29 @@
  */
 package chapter18.topic6;
 
+import java.util.concurrent.Executors;
+
 /**
  * @author matsusita
  *
  */
 public class BackgroundTask {
 	
+	private final Runnable	task;
+	
+	
+	/**
+	 * @param task runnable
+	 */
+	public BackgroundTask(Runnable task) {
+		this.task = task;
+	}
+	
 	/**
 	 * 
 	 */
 	public void invoke() {
+		Executors.newSingleThreadExecutor().execute(task);
 	}
 	
 }
--- src/test/java/chapter18/topic6/BackgroundTaskTest.java
+++ src/test/java/chapter18/topic6/BackgroundTaskTest.java
@@ -3,7 +3,16 @@
  */
 package chapter18.topic6;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 /**
  * @author matsusita
@@ -11,10 +20,29 @@ import org.junit.Test;
  */
 public class BackgroundTaskTest {
 	
+	@Rule
+	public Timeout	timeout	= new Timeout(1000);
+	
+	
 	@Test
-	public void invokeメソッドによりRunnableオブジェクトのrunメソッドが別スレッドで実行される() {
-		BackgroundTask systemUnderTest = new BackgroundTask();
+	public void invokeメソッドによりRunnableオブジェクトのrunメソッドが別スレッドで実行される() throws Exception {
+		// SetUp
+		final AtomicReference<String> backgroundThreadName = new AtomicReference<String>();
+		final CountDownLatch latch = new CountDownLatch(1);
+		Runnable task = new Runnable() {
+			
+			@Override
+			public void run() {
+				backgroundThreadName.set(Thread.currentThread().getName());
+				latch.countDown();
+			}
+			
+		};
+		BackgroundTask systemUnderTest = new BackgroundTask(task);
 		systemUnderTest.invoke();
+		latch.await();
+		// Verify
+		assertThat(backgroundThreadName.get(), is(not(Thread.currentThread().getName())));
 	}
 	
 }
